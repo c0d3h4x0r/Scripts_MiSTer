cfca6de5c58a18dd677dc0e5e3b87691ab362963
Keith F. Kelly <keith.f.kelly@outlook.com>
2024-12-16 15:33:07 -0800

    BUGFIX: Make nicer Linux terminal settings persistent

--- make_linux_terminal_nicer.sh
+++ make_linux_terminal_nicer.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-echo "Making Linux nice..."
-
 THIS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
-echo " - Modifying root user settings..."
+if [ "$1" == "stop" ]; then
+    exit 0
+fi
+
+echo "Making Linux terminal nicer."
+
+mount | grep "on / .*[(,]ro[,$]" -q && RO_ROOT="true"
+[ "$RO_ROOT" == "true" ] && mount / -o remount,rw
+
 cp -f "$THIS_DIR/nice-linux/.bashrc" /root/
 cp -f "$THIS_DIR/nice-linux/.bash_aliases" /root/
 cp -f "$THIS_DIR/nice-linux/.bash_logout" /root/
@@ -14,5 +20,12 @@ cp -f "$THIS_DIR/nice-linux/.bash_prompt" /root/
 cp -f "$THIS_DIR/nice-linux/.profile" /root/
 cp -f "$THIS_DIR/nice-linux/.vimrc" /root/
 cp -rf "$THIS_DIR/nice-linux/.ssh" /root/
+
+sync
+[ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+
+if [ -z "$1" ]; then
+    "$THIS_DIR/add_to_user_startup.sh" "$0" "Make Linux terminal nicer"
+fi
 
 echo "Done."
